5e7283f4dd54d575d04635851f27901013110048
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2023-09-18 16:11:21 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (5.74%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        
+        for(int i=n; i<300; i++) {
+            if(i%n == 0 && i%2 == 0) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};

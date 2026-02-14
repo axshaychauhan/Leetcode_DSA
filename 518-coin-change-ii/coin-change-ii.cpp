c283bc7c394b7b69393812e6c5131643e3f21cf2
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-02-14 07:28:31 +0530

    Time: 7 ms (85.20%) | Memory: 10.1 MB (96.58%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
-int solveBottomUp(int amount, vector<int>& coins) {
-    int n = coins.size();
-    // Create DP table: n rows, amount+1 columns
-    // Initialize with 0
-    vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
-
-    // Base Case: Target 0 is always 1 way
-    for (int i = 0; i < n; i++) dp[i][0] = 1;
-    
-    // Fill the table
-    for (int i = 0; i < n; i++) {
-        for (int j = 1; j <= amount; j++) {
-            long notTake = 0, take = 0;
-            // 1. Calculate 'Skip' (Not taking coin i)
-            // Be careful: if i == 0, there is no previous coin!
-            if( i != 0)
-              notTake = dp[i-1][j];
-
-            // 2. Calculate 'Take' (Taking coin i)
-            // Check if j >= coins[i] first
-            if(j >= coins[i]) 
-                take =  dp[i][j - coins[i]];
-
-            // Store sum
-            dp[i][j] = take + notTake;
-        }
-    }
-    
-    // The answer is at the last coin, full amount
-    return dp[n - 1][amount];
-}
     int change(int amount, vector<int>& coins) {
-        return solveBottomUp(amount, coins);
+        vector<unsigned int> dp(amount + 1);
+
+        dp[0] = 1;
+
+        for(int coin : coins){
+            for(int j = coin; j<= amount; ++j) {
+                dp[j] = dp[j] + dp[j - coin];
+            }
+        }
+
+        return dp[amount];
     }
 };

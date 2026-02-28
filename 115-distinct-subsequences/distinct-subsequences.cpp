9d9b11e4fdcef081828a55cf3f6d6bace2fdcd8e
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-02-28 11:46:13 +0530

    Time: 30 ms (57.87%) | Memory: 44.4 MB (22.06%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -28,7 +28,7 @@ public:
         if (dp[i][j] != -1)
             return dp[i][j];
 
-        int ans = 0;
+        long long ans = 0;
 
         if (s[i] == t[j])
             ans = solveMemoiation(s, t, i + 1, j + 1, dp) +
@@ -39,8 +39,31 @@ public:
         return dp[i][j] = ans;
     }
 
+    int solveTabulation(string s, string t) {
+
+       vector<vector<unsigned long long>> dp(s.length() + 1, vector<unsigned long long>(t.length() + 1, 0)); 
+        
+        for(int i = 0; i<=s.length(); ++i){
+            dp[i][t.length()] = 1;
+        }
+        
+        for(int i = s.length()-1; i>=0 ; --i) {
+            for(int j = t.length()-1; j>= 0; --j) {
+
+                if(s[i] == t[j])
+                   dp[i][j] = dp[i+1][j+1] + dp[i+1][j];
+                else
+                   dp[i][j] = dp[i+1][j];
+
+            }
+        }
+
+        return int(dp[0][0]);
+    }
+
     int numDistinct(string s, string t) { 
-            vector<vector<long long>> dp(s.length() + 1,
-                                 vector<long long>(t.length() + 1, -1));
-        return solveMemoiation(s, t, 0, 0, dp); }
+          //  vector<vector<long long>> dp(s.length() + 1,
+         //                        vector<long long>(t.length() + 1, -1));
+        // solveMemoiation(s, t, 0, 0, dp);
+        return solveTabulation(s,t); }
 };

22c58cbf002fd89827fb5be072219bfde47165d8
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-02 21:01:19 +0530

    Time: 7 ms (54.31%) | Memory: 17 MB (27.87%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+   bool solveRecursion(int start, string& s, unordered_set<string>& dict) {
+
+      if(start == s.length())
+         return 1;
+
+      for(int wordEnd = start + 1; wordEnd <= s.length(); ++wordEnd) {
+        string word = s.substr(start, wordEnd - start);
+
+        if(dict.count(word)) {
+            if(solveRecursion(wordEnd, s, dict) == 1)
+               return 1;
+        }
+      }
+
+      return 0;
+   }
+
+    bool solveMemoiation(int start, string& s, unordered_set<string>& dict, vector<int> & dp) {
+
+      if(start == s.length())
+         return 1;
+
+      if(dp[start] != -1)
+         return dp[start];
+
+      for(int wordEnd = start + 1; wordEnd <= s.length(); ++wordEnd) {
+        string word = s.substr(start, wordEnd - start);
+
+        if(dict.count(word)) {
+            if(solveMemoiation(wordEnd, s, dict, dp) == 1)
+               return dp[start] = 1;
+        }
+      }
+
+      return dp[start] = 0;
+   }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        vector<int> dp(s.length() + 1, -1);
+        return solveMemoiation(0, s, dict, dp);
+    }
+};

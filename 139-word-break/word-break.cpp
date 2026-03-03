51cc08c908a84922c0baf038a787269879412aa4
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-03 14:50:47 +0530

    Time: 3 ms (70.08%) | Memory: 12 MB (68.43%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -19,15 +19,19 @@ public:
 
     bool solveMemoiation(int start, string& s, unordered_set<string>& dict,
                          vector<int>& dp) {
-
+        //if index is out of bound means we succesfully found words from s in dictionary return true
         if (start == s.length())
             return 1;
 
         if (dp[start] != -1)
             return dp[start];
+
+
         string substring = "";
-        for (int wordEnd = start; wordEnd <= s.length(); ++wordEnd) {
-           substring.push_back(s[wordEnd]);
+        //make word char by char and check if it exsist in dictionary
+        //if its found then call recursion for next word
+        for (int wordEnd = start; wordEnd < s.length(); ++wordEnd) {
+            substring.push_back(s[wordEnd]);
 
             if (dict.count(substring)) {
                 if (solveMemoiation(wordEnd + 1, s, dict, dp) == 1)
@@ -38,6 +42,7 @@ public:
         return dp[start] = 0;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
+        // Added words in set to check existance in O(1) time
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         vector<int> dp(s.length() + 1, -1);
         return solveMemoiation(0, s, dict, dp);

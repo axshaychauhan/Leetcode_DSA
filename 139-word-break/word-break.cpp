5b098951394505345970921dd3224cff7549c380
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-03 14:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (66.68%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
-   bool solveRecursion(int start, string& s, unordered_set<string>& dict) {
+    bool solveRecursion(int start, string& s, unordered_set<string>& dict) {
 
-      if(start == s.length())
-         return 1;
+        if (start == s.length())
+            return 1;
 
-      for(int wordEnd = start + 1; wordEnd <= s.length(); ++wordEnd) {
-        string word = s.substr(start, wordEnd - start);
+        for (int wordEnd = start + 1; wordEnd <= s.length(); ++wordEnd) {
+            string word = s.substr(start, wordEnd - start);
 
-        if(dict.count(word)) {
-            if(solveRecursion(wordEnd, s, dict) == 1)
-               return 1;
+            if (dict.count(word)) {
+                if (solveRecursion(wordEnd, s, dict) == 1)
+                    return 1;
+            }
         }
-      }
 
-      return 0;
-   }
+        return 0;
+    }
 
-    bool solveMemoiation(int start, string& s, unordered_set<string>& dict, vector<int> & dp) {
+    bool solveMemoiation(int start, string& s, unordered_set<string>& dict,
+                         vector<int>& dp) {
 
-      if(start == s.length())
-         return 1;
+        if (start == s.length())
+            return 1;
 
-      if(dp[start] != -1)
-         return dp[start];
+        if (dp[start] != -1)
+            return dp[start];
+        string substring = "";
+        for (int wordEnd = start; wordEnd <= s.length(); ++wordEnd) {
+           substring.push_back(s[wordEnd]);
 
-      for(int wordEnd = start + 1; wordEnd <= s.length(); ++wordEnd) {
-        string word = s.substr(start, wordEnd - start);
-
-        if(dict.count(word)) {
-            if(solveMemoiation(wordEnd, s, dict, dp) == 1)
-               return dp[start] = 1;
+            if (dict.count(substring)) {
+                if (solveMemoiation(wordEnd + 1, s, dict, dp) == 1)
+                    return dp[start] = 1;
+            }
         }
-      }
 
-      return dp[start] = 0;
-   }
+        return dp[start] = 0;
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         vector<int> dp(s.length() + 1, -1);

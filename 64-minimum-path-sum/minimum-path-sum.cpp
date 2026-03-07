0bce82f0d51acff2f7a8fc090b60b45e9272f068
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-07 17:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (5.09%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -29,9 +29,25 @@ public:
         return dp[row][col] = grid[row][col] + min(solveMemoiation(row+1, col, grid, rowMax, colMax, dp), solveMemoiation(row, col+1, grid, rowMax, colMax, dp));
 
     }
+    int solveTab(int row, int col, vector<vector<int>>& grid, int rowMax, int colMax) {
+        vector<vector<long long>> dp(grid.size(), vector<long long>(grid[0].size(), INT_MAX));
+            dp[rowMax][colMax] = grid[rowMax][colMax];
+
+            for(int i = rowMax; i >=0 ; --i){
+                for(int j = colMax; j >= 0; --j) {
+                    if(i == rowMax && j == colMax)
+                       continue;
+
+                   dp[i][j] = grid[i][j] + min(i < rowMax? dp[i+1][j]: INT_MAX, j < colMax? dp[i][j+1]:INT_MAX);
+                }
+            }
+        return dp[0][0];
+    }
     int minPathSum(vector<vector<int>>& grid) {
+        return solveTab(0, 0, grid, grid.size()-1, grid[0].size()-1);
+
         vector<vector<int>> dp(grid.size() + 1, vector<int>(grid[0].size() + 1, -1));
-        return solveMemoiation(0, 0, grid, grid.size()-1, grid[0].size()-1, dp);
+        solveMemoiation(0, 0, grid, grid.size()-1, grid[0].size()-1, dp);
         solveRecursion(0, 0, grid, grid.size()-1, grid[0].size()-1);
     }
 };

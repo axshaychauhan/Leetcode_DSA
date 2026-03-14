d5f84b5862e7b2fe1235151ee27935cb8d9d1f68
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-14 11:32:21 +0530

    Time: 27 ms (58.41%) | Memory: 45.3 MB (6.50%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -26,8 +26,27 @@ public:
            return dp[i][j] = max(solveMemoiation(i+1, j, s1, s2, dp), solveMemoiation(i, j+1, s1, s2, dp));  
     }
 
+    int solveTab(string& s1, string& s2) {
+        int n = s1.length();
+        int m = s2.length();
+        
+        vector<vector<int>> dp(s1.length() + 1, vector<int>(s2.length() + 1, 0));
+        dp[n][m] = 0;
+
+        for(int i = n-1; i>= 0; --i) {
+            for(int j = m-1; j>= 0; --j) {
+                if(s1[i] == s2[j])
+                  dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                   dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        
+        return dp[0][0];
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length() + 1, -1));
-        return solveMemoiation(0, 0, text1, text2, dp);
+        return solveTab(text1, text2);
     }
 };

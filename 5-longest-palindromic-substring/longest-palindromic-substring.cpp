f426cc3eebf6e6a6044bc98ba3691f7afc3a344c
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-14 15:04:20 +0530

    Time: 6 ms (89.32%) | Memory: 9.3 MB (92.12%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    string expandAroundCenter(string s, int start, int end) {
+    int expandAroundCenter(string& s, int start, int end) {
 
         while (start >= 0 && end < s.length() && s[start] == s[end]) {
             --start;
             ++end;
         }
 
-        return s.substr(start + 1 , end - start - 1);
+        return end - start - 1;
     }
     string longestPalindrome(string s) {
-        string answer = "";
+       int start = 0;
+       int max_length = 0;
 
         for (int i = 0; i < s.length(); ++i) {
-            string oddPalin = expandAroundCenter(s, i, i);
-            string evenPalin = expandAroundCenter(s, i, i + 1);
+            int oddPalinLength = expandAroundCenter(s, i, i);
+            int evenPalinLength = expandAroundCenter(s, i, i + 1);
 
-            string tempAnswer =
-                oddPalin.length() > evenPalin.length() ? oddPalin : evenPalin;
-            answer = answer.length() > tempAnswer.length() ? answer: tempAnswer;
+            int tempMaxLength = oddPalinLength > evenPalinLength ? oddPalinLength : evenPalinLength;
+
+            if(tempMaxLength > max_length) {
+                start = i - (tempMaxLength - 1) / 2;
+                max_length = tempMaxLength;
+            }
         }
-        return answer;
+        return s.substr(start, max_length);
     }
 };

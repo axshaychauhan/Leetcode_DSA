d687881a57cebe2a81056ff8592b5e26fe75f2d5
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-15 14:39:33 +0530

    Time: 8 ms (47.03%) | Memory: 13.3 MB (30.58%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -39,10 +39,31 @@ public:
         return dp[i][j] = minOps;
         
     }
+
+     int solveTab(string& word1, string& word2, int i, int j, vector<vector<int>>& dp){
+        int m = word1.length();
+        int n = word2.length();
+
+        for(int i= 0; i<=m; i++)
+           dp[i][0] = i;
+        for(int j=0; j<=n; j++)
+           dp[0][j] = j;
+
+       for(int i = 1; i<=m; ++i) {
+        for(int j= 1; j<=n; ++j) {
+            if(word1[i-1] == word2[j-1])
+               dp[i][j] = dp[i-1][j-1];
+            else
+               dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+        }
+       }
+
+       return dp[m][n];       
+    }
     int minDistance(string word1, string word2) {
        
-        vector<vector<int>> dp(word1.length() + 1,vector<int>(word2.length() + 1, -1));
+        vector<vector<int>> dp(word1.length() + 1,vector<int>(word2.length() + 1, 0));
    
-        return solveMemoiation(word1, word2, 0, 0, dp);
+        return solveTab(word1, word2, 0, 0, dp);
     }
 };

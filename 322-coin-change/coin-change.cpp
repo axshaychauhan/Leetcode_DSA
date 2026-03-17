b5da0736658119f68ea39183aa062de3da8e2b8d
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-17 22:59:07 +0530

    Time: 27 ms (60.35%) | Memory: 22.1 MB (47.14%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,40 +1,55 @@
 class Solution {
 public:
     int solveRecursion(vector<int>& coins, int target) {
-        if(target == 0)
-           return 0;
-        if(target < 0)
-           return INT_MAX;
+        if (target == 0)
+            return 0;
+        if (target < 0)
+            return INT_MAX;
 
         int mini = INT_MAX;
-        for(int coin : coins) {
+        for (int coin : coins) {
             int ans = solveRecursion(coins, target - coin);
-            if(ans != INT_MAX)
-               mini = min(mini, 1 + ans);
+            if (ans != INT_MAX)
+                mini = min(mini, 1 + ans);
         }
         return mini;
     }
 
     int solveMemoiation(vector<int>& coins, int target, vector<int>& dp) {
-        if(target == 0)
-           return 0;
-        if(target < 0)
-           return INT_MAX;
-        if(dp[target] != -1)
-           return dp[target];
+        if (target == 0)
+            return 0;
+        if (target < 0)
+            return INT_MAX;
+        if (dp[target] != -1)
+            return dp[target];
 
         int mini = INT_MAX;
-        for(int coin : coins) {
+        for (int coin : coins) {
             int ans = solveMemoiation(coins, target - coin, dp);
-            if(ans != INT_MAX)
-               mini = min(mini, 1 + ans);
+            if (ans != INT_MAX)
+                mini = min(mini, 1 + ans);
         }
         return dp[target] = mini;
     }
 
+    int solveTab(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.size(); ++j) {
+
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX)        {
+                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+
+       return dp[amount];
+    }
+
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount + 1, -1);
-        int mini = solveMemoiation(coins, amount, dp);
-      return  mini == INT_MAX ? -1: mini;
+        int mini = solveTab(coins, amount);
+        return mini == INT_MAX ? -1 : mini;
     }
 };

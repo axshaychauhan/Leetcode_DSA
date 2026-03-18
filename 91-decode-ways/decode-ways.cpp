4628451c7af3ac50855d03a1e073ec8b30cace69
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-03-18 21:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (81.83%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int solve(string s, int i, vector<int>& dp) {
-        // base case : if we reach end of string means we found one way to
-        // decode
-        int next1 = 1;
-        int next2 = 0;
-        for (i = s.size() - 1; i >= 0; --i) {
-            // single digit
-            int curr = 0;
-
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; --i) {
             if (s[i] != '0')
-                curr += next1;
-            // double digit
+                dp[i] += dp[i + 1];
+
             if (i + 1 < s.size() &&
                 (s[i] == '1' || (s[i] == '2' && s[i + 1] < '7')))
-                curr += next2;
-
-            next2 = next1;
-            next1 = curr;
+                dp[i] += dp[i + 2];
         }
-
-        return next1;
-    }
-    int numDecodings(string s) {
-        if (s.empty())
-            return 0;
-        vector<int> dp(s.size() + 1, 0);
-        return solve(s, 0, dp);
+        return dp[0];
     }
 };

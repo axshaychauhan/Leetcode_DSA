87a165f92a8703005c1ec43bf4b64abedf132692
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-04-03 20:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.50%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
@@ -9,7 +10,7 @@ public:
             for (int c = 0; c < m; ++c) {
                 if (grid[r][c] == 2)
                     q.push({r, c});
-                else if(grid[r][c] == 1)
+                else if (grid[r][c] == 1)
                     ++freshOranges;
             }
         }
@@ -20,32 +21,24 @@ public:
                 auto [row, col] = q.front();
                 q.pop();
 
-                if (row - 1 >= 0 && grid[row - 1][col] == 1) {
-                    grid[row - 1][col] = 2;
-                    q.push({row - 1, col});
-                    --freshOranges;
-                }
-                if (row + 1 < grid.size() && grid[row + 1][col] == 1) {
-                    grid[row + 1][col] = 2;
-                    q.push({row + 1, col});
-                    --freshOranges;
-                }
-                if (col - 1 >= 0 && grid[row][col - 1] == 1) {
-                    grid[row][col - 1] = 2;
-                    q.push({row, col - 1});
-                    --freshOranges;
-                }
-                if (col + 1 < grid[0].size() && grid[row][col + 1] == 1)              {
-                    grid[row][col + 1] = 2;
-                    q.push({row, col + 1});
-                    --freshOranges;
+                for (auto dir : directions) {
+                    int newRow = row + dir.first;
+                    int newCol = col + dir.second;
+
+                    // One single, clean boundary and validation check
+                    if (newRow >= 0 && newRow < n && newCol >= 0 &&
+                        newCol < m && grid[newRow][newCol] == 1) {
+                        grid[newRow][newCol] = 2;
+                        q.push({newRow, newCol});
+                        freshOranges--;
+                    }
                 }
             }
             minutes++;
         }
-        if(freshOranges > 0)
-           return -1;
+        if (freshOranges > 0)
+            return -1;
         else
-           return minutes;
+            return minutes;
     }
 };

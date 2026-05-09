01575c19ba076d603ee5d9e9a992de3ccf0ddf06
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-05-09 19:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (31.58%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,47 +1,39 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        //create adjacency list
-        //create distance vector
-        //push source in queue as <stop,node,cost>
-        //traverse queue for nearest stops
-        //if stops > k continue
-        //check if current_cost + cost of next flight < distance[node] if yes update it
-        vector<vector<pair<int, int>>> adjList(n);
-        for(auto& flight : flights) {
-            adjList[flight[0]].push_back({flight[1], flight[2]});
+        //create adjList [[airport,price]]
+        vector<vector<pair<int,int>>> adjList(n);
+        for(auto route: flights) {
+            adjList[route[0]].push_back({route[1], route[2]});
         }
-        // vector<vector<pair<int, int>>> adjList(n);
-        // for(const auto& flight: flights)
-        //     adjList[flights[0]].push_back({flights[1], flights[2]});
 
-        vector<int> dist(n, INT_MAX);
+        //create vector to store minimum cost to reach citys
+        vector<int> minimumCost(n, INT_MAX);
 
+        //create queue which will store [currentStopCount, city, cost]
         queue<tuple<int,int,int>> q;
 
+        //push src in queue
         q.push({0, src, 0});
-        dist[src] = 0;
 
-        while(!q.empty()){
-
-            int stop = get<0>(q.front());
-            int node = get<1>(q.front());
-            int cost = get<2>(q.front());
+        //do bfs on queue
+        while(!q.empty()) {
+            auto [stops, node, currCost] = q.front();
             q.pop();
 
-            if(stop > k) continue;
-            for(auto& edge:adjList[node]){
-                int neighbour = edge.first;
-                int price = edge.second;
+            if(stops > k) continue;
+            for(auto neighbour: adjList[node]) {
+               int city = neighbour.first;
+               int price = neighbour.second;
 
-                if(cost + price < dist[neighbour]){
-                    dist[neighbour] = cost + price;
-                    q.push({stop+1, neighbour, cost + price});
-                }
+               if(currCost + price < minimumCost[city]) {
+                minimumCost[city] = currCost + price;
+                q.push({stops + 1, city, currCost + price});
+               }
             }
         }
 
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        return minimumCost[dst] == INT_MAX ? -1 : minimumCost[dst];
 
     }
 };

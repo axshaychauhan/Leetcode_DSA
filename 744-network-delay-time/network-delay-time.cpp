71c76a10d507e27190f911223046d4ccf5c003db
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-05-10 12:16:24 +0530

    Time: 85 ms (51.38%) | Memory: 44.3 MB (67.27%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        //create adjacency list
-        //create distance vector
-        //create priority queue and push k in it
-        //traverse priority queue take out top 
-        //traverse neighbours of top 
-        //if distance to reach top + neighbour to top < distance[top] update distance
-        //keep doing this unless q empty
-        //iterate distance ARRAY and return maximum distance
-
-
         vector<vector<pair<int,int>>> adjList(n+1);
-        for(const auto& edge: times) {
-            adjList[edge[0]].push_back({edge[1], edge[2]});
+        vector<int> dist(n+1, INT_MAX);
+
+        for(auto& edge: times) {
+            adjList[edge[0]].push_back({edge[1],edge[2]});
         }
 
-        vector<int> distance(n+1, INT_MAX);
-        distance[k] = 0;
-        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, k});
-
+        dist[k] = 0;
 
         while(!pq.empty()) {
-            int d = pq.top().first;
-            int node = pq.top().second;
+            auto [d, node] = pq.top(); 
             pq.pop();
 
-            if(d > distance[node]) continue;
+            if(d > dist[node]) continue;
 
-            for(const auto& neighbour: adjList[node]) {
-                int neighbourNode = neighbour.first;
-                int neighbourDistance = neighbour.second;
+            for(auto& neighbour: adjList[node]) {
+                int neigh = neighbour.first;
+                int time = neighbour.second;
 
-                if(d + neighbourDistance < distance[neighbourNode]) {
-                    distance[neighbourNode] = d + neighbourDistance;
-                    pq.push({distance[neighbourNode], neighbourNode});
+                if(d + time < dist[neigh]) {
+                    dist[neigh] = d + time;
+                    pq.push({d + time , neigh});
                 }
+
             }
         }
 
-        int minimumDelayTime = 0;
-        for(int i = 1; i<= n; ++i) {
-            if(distance[i] == INT_MAX) return -1;
-            minimumDelayTime = max(minimumDelayTime, distance[i]);
+        int minimumTime = 0;
+
+        for(int i = 1; i<=n; ++i) {
+            if(dist[i] == INT_MAX) return -1;
+            minimumTime = max(minimumTime, dist[i]);
         }
-        return minimumDelayTime;
+
+        return minimumTime;
     }
 };

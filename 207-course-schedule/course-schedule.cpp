91c0bc6ffe561338bc011fbc96494fce9343b2b7
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-05-10 15:11:05 +0530

    Time: 5 ms (54.20%) | Memory: 19.9 MB (37.78%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> adjList;
+        unordered_map<int, vector<int>> adjList(numCourses);
         vector<int> inDegree(numCourses, 0);
 
-        for (auto preq : prerequisites) {
-            int course = preq[0];
-            int prerequisite = preq[1];
-
-            adjList[prerequisite].push_back(course);
-            inDegree[course]++;
+        for(const auto& edge: prerequisites) {
+            adjList[edge[1]].push_back(edge[0]);
+            inDegree[edge[0]]++;
         }
 
         queue<int> q;
-        for (int i = 0; i < inDegree.size(); ++i) {
-            if (inDegree[i] == 0)
-                q.push(i);
+        for(int i = 0; i<numCourses; ++i) {
+            if(inDegree[i] == 0) q.push(i);
         }
-        int coursesTaken = 0;
 
-        while (!q.empty()) {
-            int size = q.size();
+        int courseTaken = 0;
+        while(!q.empty()) {
+            int course = q.front();
+            q.pop();
 
-            for (int i = 0; i < size; ++i) {
+            ++courseTaken;
 
-                int course = q.front();
-                q.pop();
-                ++coursesTaken;
-
-                auto neighbours = adjList[course];
-                for (int neighbour : neighbours) {
-                    if (--inDegree[neighbour] == 0)
-                        q.push(neighbour);
-                }
+            for(auto& neighbour: adjList[course]){
+                if(--inDegree[neighbour] == 0)
+                    q.push(neighbour);
             }
         }
 
-        return coursesTaken == numCourses;
+        return numCourses == courseTaken;
     }
 };

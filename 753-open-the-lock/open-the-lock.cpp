460d0cc05b2a5ccee93a6dff73eea87765a816a8
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-05-11 20:46:36 +0530

    Time: 117 ms (77.05%) | Memory: 38.8 MB (63.62%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -1,47 +1,45 @@
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
+        // create set of deadends to act as visited
+        // create queue and push starting state
+        // do BFS and find target while traversing increase turn for every
+        // combination
 
-        unordered_set<string> visited(
-            deadends.begin(),
-            deadends.end()); // use visited as both deadend and tracker
-        queue<string> q;
-
+        unordered_set<string> visited(deadends.begin(), deadends.end());
         string start = "0000";
-        if (visited.count(start))
-            return -1; // if start is deadend return
-
-        visited.insert(start);
+        if(visited.count(start)) return -1;
+        queue<string> q;
         q.push(start);
-        int turns = 0;
+        visited.insert(start);
 
+        int turns = 0;
         while (!q.empty()) {
 
-            int size = q.size();
+            int sz = q.size();
 
-            for (int i = 0; i < size; ++i) {
-
-                string lockNode = q.front();
+            for (int i = 0; i < sz; ++i) {
+                string comb = q.front();
                 q.pop();
-                if (lockNode == target)
-                    return turns;
-                for (int i = 0; i < 4; ++i) {
 
-                    string clockWise = lockNode;
-                    clockWise[i] = clockWise[i] == '9' ? '0' : clockWise[i] + 1;
+                if (comb == target)
+                    return turns;
+                for (int j = 0; j < 4; ++j) {
+
+                    string clockWise = comb;
+                    clockWise[j] = clockWise[j] == '9' ? '0' : ++clockWise[j];
 
                     if (!visited.count(clockWise)) {
                         visited.insert(clockWise);
                         q.push(clockWise);
                     }
 
-                    string antiClockWise = lockNode;
-                    antiClockWise[i] =
-                        antiClockWise[i] == '0' ? '9' : antiClockWise[i] - 1;
-
-                    if (!visited.count(antiClockWise)) {
-                        visited.insert(antiClockWise);
-                        q.push(antiClockWise);
+                    string anticlockWise = comb;
+                    anticlockWise[j] =
+                        anticlockWise[j] == '0' ? '9' : --anticlockWise[j];
+                    if (!visited.count(anticlockWise)) {
+                        visited.insert(anticlockWise);
+                        q.push(anticlockWise);
                     }
                 }
             }

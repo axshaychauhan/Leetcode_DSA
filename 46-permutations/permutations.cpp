79fd755717a69da75973ad4c5d102c705cbd00a8
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-06-02 08:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (69.30%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void generatePermutes(int index, vector<int>& nums, vector<vector<int>>& ans){
+        if(index == nums.size()){
+            ans.push_back(nums);
+        }
+
+        for(int i = index; i<nums.size(); ++i){
+            swap(nums[index], nums[i]);
+            generatePermutes(index + 1,nums, ans);
+            swap(nums[index], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        generatePermutes(0, nums, ans);
+        return ans;
+    }
+};

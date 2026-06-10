50ecf5a98b633e995b1feb7ff08e9d842de2f2d2
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-06-10 20:01:21 +0530

    Time: 56 ms (89.22%) | Memory: 173.2 MB (55.66%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,39 +1,34 @@
 class LRUCache {
+    private:
+        int storage = 0;
+        list<pair<int,int>> cache;
+        unordered_map<int, std::list<pair<int,int>>::iterator> mp;
 public:
-    LRUCache(int capacity) {
-        storage = capacity;
-    }
+    LRUCache(int capacity):storage(capacity) {}
     
     int get(int key) {
-        auto it = mpKeyNode.find(key);
-        if(it == mpKeyNode.end()) return -1;
+        auto it = mp.find(key);
+        if(it == mp.end()) return -1;
         cache.splice(cache.begin(), cache, it->second);
-        return  it->second->second;
+        return it->second->second;
     }
     
     void put(int key, int value) {
-        auto it = mpKeyNode.find(key);
-        if(it != mpKeyNode.end()){
+        auto it = mp.find(key);
+        if(it != mp.end()){
+            cache.splice(cache.begin(), cache,it->second);
             it->second->second = value;
-            cache.splice(cache.begin(), cache, it->second); 
         }
-        else {
-            if(storage == mpKeyNode.size()) {
-                auto itBack = cache.back();
-                mpKeyNode.erase(itBack.first);
+        else{
+            if(storage == mp.size()){
+                auto it = cache.back();
+                mp.erase(it.first);
                 cache.pop_back();
             }
-            cache.push_front(make_pair(key, value));
-            mpKeyNode[key] = cache.begin();
+            cache.push_front({key, value});
+            mp[key] = cache.begin();
         }
-        
     }
-
-    private:
-    int storage;
-    std::list<pair<int, int>> cache;
-
-    unordered_map<int, std::list<pair<int,int>>::iterator> mpKeyNode;
 };
 
 /**

75992c905cae9a1ece130311063f3a4d2c5a49fb
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-08-05 22:34:05 +0530

    Time: 23 ms (70.23%) | Memory: 17.8 MB (84.61%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount) {
-        if (amount == 0)
+    int solve(vector<int>& coins, int target) {
+        if (target == 0)
             return 0;
 
         int mini = INT_MAX;
-        for (int coin : coins) {
-            if (coin <= amount) {
-                int sub = solve(coins, amount - coin);
-                if (sub != INT_MAX)
-                    mini = min(mini, 1 + sub);
+        for (int i = 0; i < coins.size(); ++i) {
+            if (coins[i] <= target) {
+                int ans = solve(coins, target - coins[i]);
+                if (ans != INT_MAX) {
+                    mini = min(mini, 1 + ans);
+                }
             }
         }
+
         return mini;
     }
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(), coins.end());
-        vector<int> dp(amount + 1, amount + 1);
-
+        vector<int> dp(amount + 1,amount + 1);
         dp[0] = 0;
-        for (int target = 1; target <= amount; ++target) {
-            for (int coin : coins) {
-                if (coin > target)
-                    break;
-                dp[target] = min(dp[target], 1 + dp[target - coin]);
+
+        for(int t = 1; t<= amount ; ++t){
+            for(int c = 0; c < coins.size(); ++c){
+                if(coins[c] <= t){
+                    dp[t] = min(dp[t], dp[t - coins[c]] + 1);
+                }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+
+        return dp[amount] == amount + 1? -1: dp[amount];
     }
 };

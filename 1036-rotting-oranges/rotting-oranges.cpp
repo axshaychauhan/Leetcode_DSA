6c6257b393362de0fc1b96f15c2b0eb893398db5
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-08-05 21:00:21 +0530

    Time: 7 ms (5.78%) | Memory: 18.1 MB (5.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    vector<vector<int>> dir = {{0,-1},{0,1},{-1,0},{1,0}};
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int, int>> q;
-        int freshOranges = 0;
-        for (int r = 0; r < n; ++r) {
-            for (int c = 0; c < m; ++c) {
-                if (grid[r][c] == 2)
-                    q.push({r, c});
-                else if (grid[r][c] == 1)
-                    ++freshOranges;
+        queue<pair<int,int>> q;
+        int freshCount  = 0;
+        int time = 0;
+        int maxRow = grid.size();
+        int maxCol = grid[0].size();
+
+        for(int i = 0; i<maxRow; ++i){
+            for(int j = 0; j<maxCol; ++j) {
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                }
+                else if(grid[i][j] == 1){
+                    ++freshCount;
+                }
             }
         }
-        int minutes = 0;
-        while (!q.empty() && freshOranges > 0) {
-            int size = q.size();
-            for (int i = 0; i < size; ++i) {
+
+        while(freshCount > 0 && !q.empty()) {
+            int sz = q.size();
+
+            for(int i = 0; i< sz; ++i) {
                 auto [row, col] = q.front();
                 q.pop();
 
-                for (auto dir : directions) {
-                    int newRow = row + dir.first;
-                    int newCol = col + dir.second;
+                for(auto d: dir){
+                    int newRow = row + d[0];
+                    int newCol = col + d[1];
 
-                    // One single, clean boundary and validation check
-                    if (newRow >= 0 && newRow < n && newCol >= 0 &&
-                        newCol < m && grid[newRow][newCol] == 1) {
+                    if(newRow >= 0 && newRow < maxRow && newCol >= 0 && newCol < maxCol && grid[newRow][newCol] == 1) {
                         grid[newRow][newCol] = 2;
                         q.push({newRow, newCol});
-                        freshOranges--;
+                        --freshCount;
                     }
                 }
             }
-            minutes++;
+            ++time;
         }
-        if (freshOranges > 0)
-            return -1;
-        else
-            return minutes;
+
+        return freshCount == 0? time: -1;
     }
 };

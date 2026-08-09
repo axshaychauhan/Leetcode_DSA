9af23bc48b93e4f711bb8b19ad9f3e846e858c1c
axshaychauhan <103983218+axshaychauhan@users.noreply.github.com>
2026-08-09 10:30:36 +0530

    Time: 3 ms (91.40%) | Memory: 15 MB (71.47%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-    
+
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -16,24 +16,29 @@ public:
 
 class Solution {
 public:
-Node* copyRandomList(Node* head) {
-    if (!head) return nullptr;
-    // Phase 1: weave each copy in right after its original
-    for (Node* cur = head; cur; cur = cur->next->next) {
-        Node* copy = new Node(cur->val);
-        copy->next = cur->next;
-        cur->next = copy;
+    Node* copyRandomList(Node* head) {
+       if(!head) return nullptr;
+       
+        for (Node* curr = head; curr; curr = curr->next->next) {
+            Node* copy = new Node(curr->val);
+            copy->next = curr->next;
+            curr->next = copy;
+        }
+
+        for (Node* curr = head; curr; curr = curr->next->next) {
+            if (curr->random) {
+                curr->next->random = curr->random->next;
+            }
+  
+        }
+
+        Node* newHead = head->next;
+        for (Node* curr = head; curr; curr =curr->next) {
+            Node* copy = curr->next;
+            curr->next = copy->next;
+            copy->next = curr->next? curr->next->next: nullptr;
+        }
+
+        return newHead;
     }
-    // Phase 2: set randoms (copy of X is X->next)
-    for (Node* cur = head; cur; cur = cur->next->next)
-        cur->next->random = cur->random ? cur->random->next : nullptr;
-    // Phase 3: un-weave BOTH chains into separate lists
-    Node* copyHead = head->next;
-    for (Node* cur = head; cur; cur = cur->next) {
-        Node* copy = cur->next;
-        cur->next = copy->next;
-        copy->next = cur->next ? cur->next->next : nullptr;
-    }
-    return copyHead;
-}
 };
